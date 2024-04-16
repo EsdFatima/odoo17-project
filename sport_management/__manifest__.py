b707e58cc1e5c36efc320946a04714cf90296eff
EsdFatima <fessid43@gmail.com>
2024-04-16 11:19:49 +0100

    manage access rights

--- sport_management/__manifest__.py
+++ sport_management/__manifest__.py
@@ -6,10 +6,12 @@
     'license': 'LGPL-3',
     'summary': 'Management of sports',
     'description': """
-    
+    The Sport Management module in Odoo provides a comprehensive solution for schools to efficiently oversee sports activities for
+     students and athletes. It consists of three core components: activities, sessions, and time slots.
     """,
     'depends': ['base'],
     'data': [
+        'security/security.xml',
         'security/ir.model.access.csv',
         'views/sport_activity.xml',
         'views/sport_session.xml',
